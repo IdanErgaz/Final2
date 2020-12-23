cea6420c7e24293cb6ca0c47cdb3f2cfaa022f2b
user <idan.ergaz@gmail.com>
2020-12-23 09:10:28 +0200

    my first commit in my new project

---
+++ new1.py
@@ -0,0 +1,7 @@
+print("Hello world!")
+name="Idan"
+age=40
+print(f'your name is {name} and your age is:{age}')
+print("Another way!!!!!!")#####################
+print("#############################################")
+print("your name is:{} and you are {} years old".format(name, age))
