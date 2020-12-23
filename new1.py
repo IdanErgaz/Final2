1d28c4e37b4972ae71255841cae68a97ca915293
user <idan.ergaz@gmail.com>
2020-12-23 09:18:17 +0200

    adding a new line to the filegit add . !

--- new1.py
+++ new1.py
@@ -5,3 +5,4 @@ print(f'your name is {name} and your age is:{age}')
 print("Another way!!!!!!")#####################
 print("#############################################")
 print("your name is:{} and you are {} years old".format(name, age))
+print("adding a NEW text - check auto test run after commit !!!!!")
